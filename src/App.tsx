7c673e8bd921c50c7b9793515f38906a3f98aa7f
Reda Hilal <reda0x04@gmail.com>
2025-10-03 07:44:44 +0400

    install chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, HStack, Stack } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello</h1>
+      <HStack>
+        <Button colorScheme="teal">Click me</Button>
+        <Button colorScheme="blue">Click me</Button>
+        <Stack direction="row" gap="4" align="center">
+          <Button loading>Click me</Button>
+          <Button loading loadingText="Saving...">
+            Click me
+          </Button>
+        </Stack>
+      </HStack>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
