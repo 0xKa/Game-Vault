0d7fdf5ef49ad6b854e4f75ca867f9b710b9b1d3
Reda Hilal <reda0x04@gmail.com>
2025-10-11 20:12:35 +0400

    load from static data instead of server

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,10 @@
 import type { Genre } from "@/types";
 import useData from "./useData";
+import genres from "@/data/genres-static";
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({ data: genres, isLoading: false, error: null }); // for loading static data
+
+// uncomment the line below to fetch from the API
+// const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,10 @@
 import type { Platform } from "@/types";
 import useData from "./useData";
+import platforms from "@/data/platforms-static";
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => ({ data: platforms, isLoading: false, error: null }); // for loading static data
+
+// uncomment the line below to fetch from the API
+// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 
 export default usePlatforms;
